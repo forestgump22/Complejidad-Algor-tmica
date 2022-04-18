921189f16424d67ebedb42961003384053327312
IbrahimImanol <89035983+IbrahimImanol@users.noreply.github.com>
2022-04-17 22:12:10 -0500

    Update sesion6.py

--- sesion6.py
+++ sesion6.py
@@ -4,7 +4,7 @@ def fibonnaci(i):
     memo={};
     memo[0]=1;
     memo[1]=1;
-    def dp(i):
+    def dp(i):  
         if i not in memo:
             memo[i]=dp(i-1)+dp(i-2);
         return memo[i];
